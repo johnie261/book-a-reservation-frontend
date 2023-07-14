b3d4cc018b70b10218901ed95872d72771fe4b2a
johnie261 <njorogejohn261@gmail.com>
2023-07-14 11:40:05 +0300

    Add a folder that contains files that have array for the link name and icons

---
+++ src/Utils/social.js
@@ -0,0 +1,32 @@
+import {
+  FaFacebookF,
+  FaInstagram,
+  FaTwitter,
+  FaWhatsapp,
+  FaYoutube,
+} from 'react-icons/fa';
+
+const social = [
+  {
+    id: 1,
+    icon: <FaTwitter />,
+  },
+  {
+    id: 2,
+    icon: <FaFacebookF />,
+  },
+  {
+    id: 3,
+    icon: <FaInstagram />,
+  },
+  {
+    id: 4,
+    icon: <FaYoutube />,
+  },
+  {
+    id: 5,
+    icon: <FaWhatsapp />,
+  },
+];
+
+export default social;
