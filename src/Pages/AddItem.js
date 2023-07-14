40991dba3bac7deaffe4ea99e1c6a926744f4f17
johnie261 <njorogejohn261@gmail.com>
2023-07-14 23:59:04 +0300

    Create a form to add new glamping

--- src/Pages/AddItem.js
+++ src/Pages/AddItem.js
@@ -1,7 +1,71 @@
 import React from 'react';
 
 const AddItem = () => (
-  <div>A Form to add a new item</div>
+  <div className="form-container">
+    <h2 className="form-name">ADD A NEW GLAMPING</h2>
+    <div className="underline" />
+    <form className="item-form">
+      <label htmlFor="name">
+        Name:
+        <input
+          id="name"
+          name="name"
+          type="text"
+          value=""
+          className="form-input"
+        />
+      </label>
+      <label htmlFor="location">
+        Location:
+        <input
+          id="location"
+          name="location"
+          type="text"
+          value=""
+          className="form-input"
+        />
+      </label>
+      <label htmlFor="type">
+        Gampling Type:
+        <input
+          id="type"
+          name="type"
+          type="text"
+          value=""
+          className="form-input"
+        />
+      </label>
+      <label htmlFor="description">
+        Description:
+        <textarea
+          id="description"
+          name="description"
+          value=""
+          className="form-input"
+        />
+      </label>
+      <label htmlFor="image">
+        Image link:
+        <input
+          id="image"
+          name="image"
+          type="text"
+          className="form-input"
+        />
+      </label>
+      <label htmlFor="dailyRate">
+        Daily Rate:
+        <input
+          id="dailyRate"
+          name="dailyRate"
+          type="text"
+          value=""
+          className="form-input"
+        />
+      </label>
+      <button type="button">Add Gampling</button>
+    </form>
+  </div>
 );
 
 export default AddItem;
