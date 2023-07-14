5fc646f66ba7302bec9f2dec4c8d14838f4197bb
blueberry1312 <nestor.oro.alvarez@gmail.com>
2023-07-14 15:33:10 -0400

    Add store to app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './Pages/Home';
 import Reservations from './Pages/Reservations';
@@ -9,17 +9,15 @@ import DeleteItem from './Pages/DeleteItem';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="/reserve" element={<Reserve />} />
-          <Route path="/reservations" element={<Reservations />} />
-          <Route path="/add-item" element={<AddItem />} />
-          <Route path="/delete-item" element={<DeleteItem />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="/reserve" element={<Reserve />} />
+        <Route path="/reservations" element={<Reservations />} />
+        <Route path="/add-item" element={<AddItem />} />
+        <Route path="/delete-item" element={<DeleteItem />} />
+      </Route>
+    </Routes>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import store from './store';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById('root'),
 );
