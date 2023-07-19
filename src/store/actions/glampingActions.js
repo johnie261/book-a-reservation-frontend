60bf4fc3f5d668ecbdcfd483b01815d0acb1f8e9
blueberry1312 <nestor.oro.alvarez@gmail.com>
2023-07-18 21:09:44 -0400

    Update glampings list actions

--- src/store/actions/glampingActions.js
+++ src/store/actions/glampingActions.js
@@ -1,24 +1,10 @@
 import axios from 'axios';
 
-export const FETCH_GLAMPINGS_REQUEST = 'FETCH_GLAMPINGS_REQUEST';
-export const FETCH_GLAMPINGS_SUCCESS = 'FETCH_GLAMPINGS_SUCCESS';
-export const FETCH_GLAMPINGS_FAILURE = 'FETCH_GLAMPINGS_FAILURE';
-
-export const fetchGlampings = () => (dispatch) => {
-  dispatch({ type: FETCH_GLAMPINGS_REQUEST });
-
-  axios
-    .get('http://localhost:3000/glampings/list_glampings')
-    .then((response) => {
-      // Modify the response data to match the expected shape
-      const modifiedData = response.data.map(([name, glampingType]) => ({
-        name,
-        glampingType,
-      }));
-
-      dispatch({ type: FETCH_GLAMPINGS_SUCCESS, payload: modifiedData });
-    })
-    .catch((error) => {
-      dispatch({ type: FETCH_GLAMPINGS_FAILURE, payload: error.message });
-    });
+export const fetchGlampings = () => async (dispatch) => {
+  try {
+    const response = await axios.get('http://localhost:3000/glampings/list_glampings');
+    dispatch({ type: 'FETCH_GLAMPINGS_SUCCESS', payload: response.data });
+  } catch (error) {
+    dispatch({ type: 'FETCH_GLAMPINGS_FAILURE', payload: error.message });
+  }
 };
