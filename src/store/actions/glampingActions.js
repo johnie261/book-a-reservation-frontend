8046ae0bf5a2231d94ee575ea6fced45b1ce5a05
blueberry1312 <nestor.oro.alvarez@gmail.com>
2023-07-18 21:10:49 -0400

    Add actions to glamping details

--- src/store/actions/glampingActions.js
+++ src/store/actions/glampingActions.js
@@ -8,3 +8,12 @@ export const fetchGlampings = () => async (dispatch) => {
     dispatch({ type: 'FETCH_GLAMPINGS_FAILURE', payload: error.message });
   }
 };
+
+export const fetchGlampingDetails = (id) => async (dispatch) => {
+    try {
+      const response = await axios.get(`http://localhost:3000/glampings/list_glampings_details/${id}`);
+      dispatch({ type: 'FETCH_GLAMPING_DETAILS_SUCCESS', payload: response.data });
+    } catch (error) {
+      dispatch({ type: 'FETCH_GLAMPING_DETAILS_FAILURE', payload: error.message });
+    }
+  };
