906283d457809b42764e58d2dfe9c948bbe03df4
blueberry1312 <nestor.oro.alvarez@gmail.com>
2023-07-18 22:37:00 -0400

    Finish design to glamping list, refactor to new assets folder

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,22 +1,35 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchGlampings } from '../store/actions/glampingActions';
+import '../assets/Home.css';
 
 const Home = () => {
   const dispatch = useDispatch();
   const glampingsList = useSelector((state) => state.glampings.glampingsList);
 
+  const [currentIndex, setCurrentIndex] = useState(0);
+
   useEffect(() => {
     dispatch(fetchGlampings());
   }, [dispatch]);
+
+  const handlePrev = () => {
+    setCurrentIndex((prevIndex) => (prevIndex > 0 ? prevIndex - 1 : glampingsList.length - 3));
+  };
+
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) => (prevIndex < glampingsList.length - 3 ? prevIndex + 1 : 0));
+  };
+
+  const visibleGlampings = glampingsList.slice(currentIndex, currentIndex + 3);
 
   return (
     <div className="glamping-list">
       <h1>Glampings</h1>
       <h4>Please select a glamping.</h4>
       <div className="glamping-carousel">
-        {glampingsList.map((glamping) => (
+        {visibleGlampings.map((glamping) => (
           <div className="glamping-item" key={glamping[0]}>
             <Link to={`/glamping/${glamping[0]}`}>
               {' '}
@@ -26,6 +39,14 @@ const Home = () => {
             <p className="glamping-type">{glamping[2]}</p>
           </div>
         ))}
+      </div>
+      <div className="carousel-navigation">
+        <button aria-label="Previous" className="arrow arrow-left" type="button" onClick={handlePrev}>
+          &#8249;
+        </button>
+        <button aria-label="Next" className="arrow arrow-right" type="button" onClick={handleNext}>
+          &#8250;
+        </button>
       </div>
     </div>
   );
