8bdcb1d0f73f7a737c12e0b1575045a831bf9888
johnie261 <njorogejohn261@gmail.com>
2023-07-19 03:55:02 +0300

    Fix linters

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
         </Route>
       </Routes>
       <ToastContainer position="top-center" />
-    </> 
+    </>
   );
 }
 
--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,10 +1,10 @@
 import { combineReducers } from 'redux';
 import glampingReducer from './glampingReducer';
-import glampingSlice from '../../Features/gampling/gamplingSlice'
+import glampingSlice from '../../Features/gampling/gamplingSlice';
 
 const rootReducer = combineReducers({
   glamping: glampingReducer,
-  glampingForm: glampingSlice
+  glampingForm: glampingSlice,
 });
 
 export default rootReducer;
