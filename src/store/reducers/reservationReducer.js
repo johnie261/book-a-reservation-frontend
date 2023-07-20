388061357c024021eea6cf730ba5f20672e09958
blueberry1312 <nestor.oro.alvarez@gmail.com>
2023-07-20 15:41:44 -0400

    Add reducer to reservation

---
+++ src/store/reducers/reservationReducer.js
@@ -0,0 +1,17 @@
+const initialState = {
+    reservations: [],
+    error: null,
+  };
+  
+  const reservationReducer = (state = initialState, action) => {
+    switch (action.type) {
+      case 'FETCH_RESERVATIONS_SUCCESS':
+        return { ...state, reservations: action.payload, error: null };
+      case 'FETCH_RESERVATIONS_FAILURE':
+        return { ...state, reservations: [], error: action.payload };
+      default:
+        return state;
+    }
+  };
+  
+  export default reservationReducer;
