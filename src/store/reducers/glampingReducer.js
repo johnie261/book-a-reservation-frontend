4a596759ea8106d34f68ee73d91c62e45851d12f
johnie261 <njorogejohn261@gmail.com>
2023-07-20 19:24:38 +0300

    Add a delete gampling reducers and also added an isloading state

--- src/store/reducers/glampingReducer.js
+++ src/store/reducers/glampingReducer.js
@@ -1,11 +1,39 @@
 const initialState = {
   glampingsList: [],
+  isLoading: true,
   glampingDetails: null,
   error: null,
 };
 
 const glampingsReducer = (state = initialState, action) => {
   switch (action.type) {
+    case 'FETCH_GLAMPINGS_REQUEST':
+      return { ...state, isLoading: true, error: null };
+    case 'FETCH_GLAMPINGS_SUCCESS':
+      return { ...state, glampingsList: action.payload, isLoading: false, error: null };
+    case 'FETCH_GLAMPINGS_FAILURE':
+      return { ...state, glampingsList: [], isLoading: false, error: action.payload };
+    case 'FETCH_GLAMPING_DETAILS_REQUEST':
+      return { ...state, isLoading: true, error: null };
+    case 'FETCH_GLAMPING_DETAILS_SUCCESS':
+      return { ...state, glampingDetails: action.payload, isLoading: false, error: null };
+    case 'FETCH_GLAMPING_DETAILS_FAILURE':
+      return { ...state, glampingDetails: null, isLoading: false, error: action.payload };
+    case 'DELETE_GLAMPING':
+      const deletedItemId = action.payload
+      return {
+        ...state,
+        glampingsList: state.glampingsList.filter((item) => item.id !== deletedItemId)
+      }
+    default:
+      return state;
+  }
+};
+
+export default glampingsReducer;
+
+/*
+switch (action.type) {
     case 'FETCH_GLAMPINGS_SUCCESS':
       return { ...state, glampingsList: action.payload, error: null };
     case 'FETCH_GLAMPINGS_FAILURE':
@@ -17,6 +45,4 @@ const glampingsReducer = (state = initialState, action) => {
     default:
       return state;
   }
-};
-
-export default glampingsReducer;
+*/
