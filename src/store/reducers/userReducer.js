4b467e9e4fdae169e943fb6249cca2aa3cb0e8af
blueberry1312 <nestor.oro.alvarez@gmail.com>
2023-07-20 15:41:21 -0400

    Add reducer to login

---
+++ src/store/reducers/userReducer.js
@@ -0,0 +1,19 @@
+const initialState = {
+    username: '',
+    error: null,
+  };
+  
+  const userReducer = (state = initialState, action) => {
+    switch (action.type) {
+      case 'LOGIN_SUCCESS':
+        return { ...state, username: action.payload, error: null };
+      case 'LOGIN_FAILURE':
+        return { ...state, error: action.payload };
+      case 'LOGOUT':
+        return { ...state, username: 'guest', error: null };
+      default:
+        return state;
+    }
+  };
+  
+  export default userReducer;
