8857e8511082422b585d6883e87b64722358ca9e
blueberry1312 <nestor.oro.alvarez@gmail.com>
2023-07-20 15:42:39 -0400

    Update store

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,14 @@ import { createStore, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import glampingsReducer from './reducers/glampingReducer';
 import glampingSlice from '../Features/gampling/gamplingSlice';
+import userReducer from './reducers/userReducer';
+import reservationReducer from './reducers/reservationReducer';
 
 const rootReducer = combineReducers({
   glampings: glampingsReducer,
   glampingForm: glampingSlice,
+  user: userReducer,
+  reservations: reservationReducer,
 });
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
