5dd0afb84717aa4e677da880511f5ae14b696f72
johnie261 <njorogejohn261@gmail.com>
2023-07-21 20:40:50 +0300

    Add a modal to open when screen size is less thsn 991px

--- src/Components/Navlinks.js
+++ src/Components/Navlinks.js
@@ -1,7 +1,7 @@
 import { NavLink } from 'react-router-dom';
 import links from '../Utils/links';
 
-const Navlinks = () => (
+const Navlinks = ({onClick}) => (
   <div className="nav-links">
     {links.map((link) => {
       const { id, text, path } = link;
@@ -9,6 +9,7 @@ const Navlinks = () => (
         <NavLink
           to={path}
           key={id}
+          onClick={onClick}
           className="nav-link"
         >
           <h4>{text}</h4>
