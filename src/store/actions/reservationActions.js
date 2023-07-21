f95359b351abd175e15fa6f2c04c3a1bed7c540a
blueberry1312 <nestor.oro.alvarez@gmail.com>
2023-07-21 15:52:45 -0400

    Add glampingdetails on fetchreservations

--- src/store/actions/reservationActions.js
+++ src/store/actions/reservationActions.js
@@ -6,7 +6,21 @@ const fetchReservations = (username) => async (dispatch) => {
     const filteredReservations = response.data.filter(
       (reservation) => reservation[3] === username,
     );
-    dispatch({ type: 'FETCH_RESERVATIONS_SUCCESS', payload: filteredReservations });
+    const reservationsWithGlampingDetails = await Promise.all(
+      filteredReservations.map(async (reservation) => {
+        const glampingId = reservation[4];
+        const glampingDetailsResponse = await axios.get(`http://localhost:3000/glampings/list_glampings_details/${glampingId}`);
+        const glampingDetails = glampingDetailsResponse.data;
+
+        return {
+          ...reservation,
+          glampingName: glampingDetails.name,
+          glampingCity: glampingDetails.location,
+        };
+      }),
+    );
+
+    dispatch({ type: 'FETCH_RESERVATIONS_SUCCESS', payload: reservationsWithGlampingDetails });
   } catch (error) {
     console.error('Error fetching reservations:', error);
     dispatch({ type: 'FETCH_RESERVATIONS_FAILURE', payload: 'Error fetching reservations.' });
