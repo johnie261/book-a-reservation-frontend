98f5569dc35789a926d672ba2cf00b5f8e126a07
johnie261 <njorogejohn261@gmail.com>
2023-07-21 20:44:28 +0300

    Add styling to the modal

--- src/Pages/Layout.js
+++ src/Pages/Layout.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Sidebar from '../Components/Sidebar';
+import ModalSidebar from '../Components/ModalSidebar';
 
 const Layout = () => (
   <div className="layout">
     <Sidebar />
+    <ModalSidebar />
     <main className="outlet">
       <Outlet />
     </main>
