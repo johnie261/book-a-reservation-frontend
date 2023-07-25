47689da22aa39679386a53f61e28f1014873a2c1
johnie261 <njorogejohn261@gmail.com>
2023-07-25 22:37:59 +0300

    Create an array containing input values

---
+++ src/Utils/FormInputs.js
@@ -0,0 +1,40 @@
+const inputFields = [
+  {
+    name: 'name',
+    label: 'Name:',
+    type: 'text',
+    placeholder: 'Enter name of the glamping',
+  },
+  {
+    name: 'location',
+    label: 'Location:',
+    type: 'text',
+    placeholder: 'Enter location',
+  },
+  {
+    name: "glamping_type",
+    label: 'Glamping Type:',
+    type: 'text',
+    placeholder: 'Enter type of glamping',
+  },
+  {
+    name: 'description',
+    label: 'Description:',
+    type: 'textarea',
+    placeholder: 'Add description',
+  },
+  {
+    name: 'image',
+    label: 'Image link:',
+    type: 'text',
+    placeholder: 'Add a link to an image',
+  },
+  {
+    name: 'daily_rate',
+    label: 'Daily Rate:',
+    type: 'number',
+    placeholder: 'Add rate',
+  },
+];
+
+export default inputFields;
